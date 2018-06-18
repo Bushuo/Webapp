ff0ae395f9c83ef2896f127838a6f6ff213d9a5b
BushuoPaul Buschmann <paul.buschmann@chello.at>
2018-06-18 23:09:47 +0200

    data structure to store expenses and incomes

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -1,15 +1,36 @@
 var budgetController = (function() {
+    var Expense = function(id,description,value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id,description,value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
 }());
 
 
 var uiController = (function() {
-	
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: 'add__description',
         inputValue: '.add__value',
-        inputBtn: 'add__btn'
+        inputBtn: '.add__btn'
     };
 
     return {
@@ -20,7 +41,7 @@ var uiController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         
-        }
+        },
 
         getDOMstrings: function() {
             return DOMstrings;
@@ -30,14 +51,25 @@ var uiController = (function() {
 
 
 var  appController = (function(budgetCtrl, uiCtrl) {
+    
+    var setupEventListeners = function() {
+        var DOM = uiCtrl.getDOMstrings();    
+        document.querySelector(DOM.inputBtn).addEventListener("click", function() {
+            ctrlAddItem();
+        });
+
+        document.addEventListener("keypress", function(event) {
+            if(event.keyCode === 13) {
+                ctrlAddItem();
+	        }
+        });
+    };
 
     var ctrlAddItem = function() {
         // 1. Get input data
         var input = uiCtrl.getInput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
-        
 
         // 3. Add the new item to the UI
 
@@ -45,15 +77,13 @@ var  appController = (function(budgetCtrl, uiCtrl) {
 
         // 5. Display the budget on the UI
     };
-
-    document.querySelector(".add__btn").addEventListener("click", function() {
-        ctrlAddItem();
-    });
-
-    document.addEventListener("keypress", function(event) {
-        if(event.keyCode === 13) {
-            ctrlAddItem();
-	}
-	
-    });
+    
+    return {
+        init: function() {
+            console.log('App has started');
+            setupEventListeners();
+        }
+    };
 }(budgetController, uiController));
+
+appController.init();
