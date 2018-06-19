9b00a39dff25e0dd2fcba6931f9126b4cb4c86c8
BushuoPaul Buschmann <paul.buschmann@chello.at>
2018-06-19 13:45:05 +0200

    display the budget and init placeholders

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -94,7 +94,11 @@ var uiController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer:'.income__list',
-        expensesContainer:'.expenses__list'
+        expensesContainer:'.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -147,6 +151,18 @@ var uiController = (function() {
             fieldsArray[0].focus();
         },
 
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+            }
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -174,10 +190,12 @@ var  appController = (function(budgetCtrl, uiCtrl) {
 
         // 1. Calc the budget
         budgetCtrl.calculateBudget();
+
         // 2. Return the budget
         budget = budgetCtrl.getBudget();
+
         // 3. Display the budget on the UI
-        console.log(budget);
+        uiCtrl.displayBudget(budget);
     }
     
     var ctrlAddItem = function() { var input, newItem;
@@ -204,6 +222,12 @@ var  appController = (function(budgetCtrl, uiCtrl) {
         init: function() {
             console.log('App has started');
             setupEventListeners();
+            uiCtrl.displayBudget({
+                budget:0,
+                percentage:-1,
+                totalInc:0,
+                totalExp:0
+            });
         }
     };
 }(budgetController, uiController));
